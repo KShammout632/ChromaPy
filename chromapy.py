5f1cab6fdc5914939d8bceba68049bafb76c57b0
Kareem Shammout <kareemshammout632@gmail.com>
2020-12-15 08:31:00 -0800

    Implemented fit function for model, reorganized preprocessing methods

--- chromapy.py
+++ chromapy.py
@@ -5,30 +5,50 @@ import torch.optim as optim
 import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
+from torch.utils import data
 from skimage import color
 from PIL import Image
 import matplotlib.pyplot as plt
 from cnn_model import Model
+import pickle
+from keras.datasets import cifar10
+from sklearn.model_selection import train_test_split
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--image", type=str, required=True,
+        help="path to input black and white image")
+    parser.add_argument('--use_gpu', action='store_true', default=False, 
+        help='whether to use GPU')
+    return parser.parse_args()
+
+def preprocess_training_set(train):
+    processed_x = []
+    processed_y = []
+    for image in train:
+        l, ab = preprocess_image(image)
+        processed_x.append(l)
+        processed_y.append(ab)
+    return processed_x, processed_y
 
 def preprocess_image(img, height=256, width=256):
     """Return the light intensity part of an image, resized and converted to tensor"""
-    image = Image.open(img).convert('RGB')
-    image_r = image.resize((width, height))
-    image_r_np = np.array(image_r) / 255.0
-    
+    # image = Image.open(img).convert('RGB')
+    # image_r = image.resize((width, height))
+    image_r_np = np.array(img) / 255.0
     # Convert image to Lab format
     image_lab = color.rgb2lab(image_r_np)
     # Extract L dimension
     image_l = image_lab[:,:,0]
+    image_ab = image_lab[:,:,1:]
     # Convert to tensor and add relevant dimensions
-    tens_l = torch.Tensor(image_l)[None,None,:,:]
+    image_l = image_l[None,:,:]
 
-    return tens_l
+    return image_l, image_ab
 
 def postprocess_tens(orig_img, ab, mode='bilinear'):
 	# orig_img 	1 x 1 x H_orig x W_orig
 	# ab 		1 x 2 x H x W
-
     HW_orig = orig_img.shape[2:]
     HW = ab.shape[2:]
 
@@ -39,25 +59,50 @@ def postprocess_tens(orig_img, ab, mode='bilinear'):
         ab_orig = ab
 
     out_lab_orig = torch.cat((orig_img, ab_orig), dim=1)
-    return color.lab2rgb(out_lab_orig.data.cpu().numpy()[0,...].transpose((1,2,0)))
+    out_lab_orig = out_lab_orig.data.cpu().numpy()
+    return color.lab2rgb(out_lab_orig[0,...].transpose((1,2,0)))
 
-# Parse arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--image", type=str, required=True,
-	help="path to input black and white image")
-parser.add_argument('--use_gpu', action='store_true', default=False, 
-    help='whether to use GPU')
-args = parser.parse_args()
+args = parse_arguments()
+# image_dict = unpickle('C:\\Users\\karee\\Desktop\\ChromaPy\\data\\cifar-10-python\\cifar-10-batches-py\\data_batch_1')
+# print(image_dict[b'data'])
+(X, y), (x_test, y_test) = cifar10.load_data()
 
-preprocessed_tensor = preprocess_image(args.image)
+# Split data into training and validation
+x_train, x_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# print(prepocessed_tensor.shape)
+og_image = x_train[1]
 
-model = Model().eval()
-ab_out = model.forward(preprocessed_tensor)
-print(ab_out.shape)
+x_train, y_train = preprocess_training_set(x_train[:10])
+x_val, y_val = preprocess_training_set(x_val[:10])
 
-image_new = postprocess_tens(preprocessed_tensor, ab_out)
+tensor_x_train = torch.Tensor(x_train).float()
+tensor_x_val = torch.Tensor(x_val).float()
+tensor_y_train = torch.Tensor(y_train).permute(0,3,1,2).float()
+tensor_y_val = torch.Tensor(y_val).permute(0,3,1,2).float()
 
-plt.imshow(image_new)
+# Dataset dictionary
+dsets = {
+    "train": data.TensorDataset(tensor_x_train,tensor_y_train),
+    "val": data.TensorDataset(tensor_x_val,tensor_y_val)}
+
+dataloaders = {x : data.DataLoader(dsets[x], batch_size=6, shuffle=True)
+                for x in ['train', 'val']}
+
+dataset_sizes = {x : len(dsets[x]) for x in ["train","val"]}
+
+# preprocessed_tensor = preprocess_image(args.image)
+model = Model()
+
+model_ft = model.fit(dataloaders, 4)
+# ab_out = model.forward(preprocessed_tensor)
+ab_out = model_ft.forward(tensor_x_train[1][None,:,:,:])
+# print(ab_out.shape)
+
+image_new = postprocess_tens(tensor_x_train[1][None,:,:,:], ab_out)
+
+f, axarr = plt.subplots(2,2)
+axarr[0,0].imshow(og_image)
+axarr[0,1].imshow(image_new)
+axarr[1,0].imshow(og_image)
+axarr[1,1].imshow(image_new)
 plt.show()
--- cnn_model.py
+++ cnn_model.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
+import time
+import copy
+import numpy as np
 
 
 class Model(nn.Module):
@@ -29,6 +32,92 @@ class Model(nn.Module):
             nn.ConvTranspose2d(64,2,kernel_size=5),
             nn.ReLU(True)
         )
+        
+    def fit(self, dataloaders, num_epochs):
+        optimizer = optim.Adam(self.parameters(), lr=0.001)
+        # Reduces learning rate when less progess is made
+        scheduler = optim.lr_scheduler.StepLR(optimizer, 4)
+        # Loss function
+        criterion = nn.MSELoss()
+        since = time.time()
+        best_model_wts = copy.deepcopy(self.state_dict())
+        best_acc = 0.0
+        valid_loss_min = np.Inf
+
+        for epoch in range(num_epochs):
+            print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+            print('-' * 10)
+            
+            train_loss = 0.0
+            valid_loss = 0.0
+            
+            valid_acc = 0
+            
+            # Set model to train
+            self.train()
+            
+            for j, (inputs, true) in enumerate(dataloaders['train']):
+                    # clear all gradients since gradients get accumulated after every iteration.
+                    optimizer.zero_grad()
+                    
+                    outputs = self.forward(inputs)
+                    # Calculates the loss                           
+                    loss = criterion(outputs, true)
+                    
+                    # Backpropagation
+                    loss.backward()
+                    # Update parameters
+                    optimizer.step()
+                
+                    train_loss += loss.item() * inputs.size(0)
+                    
+                    size = len(dataloaders['train'])
+                    
+                    train_loss = train_loss / size
+                    
+                    print(f'Epoch: {epoch}\t{100 * (j + 1) / size:.2f}% complete.\n', end='\r')
+            
+            # Do scheduler step after learning rate step
+            scheduler.step()
+
+            # Validation phase
+            with torch.no_grad():
+                # Set to evaluation mode
+                self.eval()
+                
+                for inputs, labels in dataloaders['val']:                                
+                    outputs = self.forward(inputs)
+                    # Calculates the loss                          
+                    loss = criterion(outputs, labels)
+
+                    valid_loss += loss.item() * inputs.size(0)
+
+                    size = len(dataloaders['val'])
+                    
+                    train_loss = train_loss / size
+
+                # Calculate average losses
+                train_loss = train_loss / len(dataloaders['train'])
+                valid_loss = valid_loss / len(dataloaders['val'])
+                    
+                print(f'\nEpoch: {epoch} \tTraining Loss: {train_loss:.4f} \tValidation Loss: {valid_loss:.4f}')
+                    
+                # Copy the model if it has a better validation loss
+                if valid_loss < valid_loss_min:
+                    valid_loss_min = valid_loss
+                    best_acc = valid_acc
+                    best_model_wts = copy.deepcopy(self.state_dict())
+                    
+        time_elapsed = time.time() - since
+        print('Training complete in {:.0f}m {:.0f}s'.format(
+            time_elapsed // 60, time_elapsed % 60))
+        print('Best val Acc: {:4f}'.format(best_acc))
+        
+        # Load best model parameters 
+        self.load_state_dict(best_model_wts)
+        torch.save(self.state_dict(), 'C:\\Users\\karee\\Desktop\\ChromaPy\\model.pth')
+        # Return model
+        return self
 
     def forward(self, x):
         x = self.encoder(x)
